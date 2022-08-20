063dfc7473ce9fc69efdfac34990a389b5117974
Higas01 <higor.porangaba123@hotmail.com>
2022-08-20 05:47:10 -0300

    app-clima

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const input = document.querySelector('input');
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
-    console.log(input.value);
     axios(`https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input.value)}&appid=ec12a89e4e02bfa66ad0671d31ccd7cb&units=metric&lang=pt_br`)
 .then(response => cityWeather(response.data))
 .catch(error => alert('Cidade Inválida!'))
@@ -11,8 +10,19 @@ button.addEventListener('click', () => {
 
 const cityWeather = response => {
 const displayTemperature = document.querySelector('.temperatura');
-const img = document.querySelector('img');
 const {main, weather} = response;
+weatherImage(response);
+
+
+return displayTemperature.innerHTML = `<p> Cidade: ${response.name}</p> <p>Temperatura: ${Math.floor(main.temp)}°C</p> <p> Clima: ${weather[0].description}`
+}
+
+const weatherImage = response => {
+
+const img = document.querySelector('img');
+
+const {weather} = response;
+
 
 if(weather[0].description === 'algumas nuvens') img.src = '/img/danieledesantis-weather-icons-cloudy-3.svg'
 
@@ -24,9 +34,8 @@ else if(weather[0].description === 'nuvens dispersas') img.src = '/img/danielede
 
 else img.src = 'danieledesantis-weather-icons-rainy-2'
 
-
-return displayTemperature.innerHTML = `<p> Cidade: ${response.name}</p> <p>Temperatura: ${Math.floor(main.temp)}°C</p> <p> Clima: ${weather[0].description}`
 }
+
 
 
 
